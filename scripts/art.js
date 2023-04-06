f219b79a539e9f15316a8e5409cb170595e35afb
Jiecheng Chen <jasonarong96@gmail.com>
2023-04-06 12:57:36 -0700

    fix video for ios

--- scripts/art.js
+++ scripts/art.js
@@ -4,7 +4,7 @@ export function changeVideo(name){
     }
     
 
-    let videoSrc = document.querySelector('#videoDescription video source');
+    let videoSrc = document.querySelectorAll('#videoDescription video source');
     let video = document.querySelector('#videoDescription video');
 
     let title = document.querySelector('#videoDescription h3');
@@ -13,7 +13,8 @@ export function changeVideo(name){
 
     if(name == 'mv'){
         console.log('change to mv');
-        videoSrc.setAttribute('src', './media/OneLastKissMV.webm');
+        videoSrc[0].setAttribute('src', './media/OneLastKissMV.webm');
+        videoSrc[1].setAttribute('src', './media/OneLastKissMV.mov');
         video.setAttribute('poster', './images/artGallery/MV.jpeg');
         title.innerText = 'One Last Kiss';
         smallTitle.innerHTML = 'Editing <br>Feb 10, 2021';
@@ -24,7 +25,8 @@ export function changeVideo(name){
     }
     else if(name == 'kitchen'){
         console.log('change to kitchen');
-        videoSrc.setAttribute('src', './media/KevinsKitchen.webm');
+        videoSrc[0].setAttribute('src', './media/KevinsKitchen.webm');
+        videoSrc[1].setAttribute('src', './media/KevinsKitchen.mp4');
         video.setAttribute('poster', './images/artGallery/Kitchen.jpg');
         title.innerText = 'Kevin\'s Kitchen';
         smallTitle.innerHTML = 'Filming · Editing <br>Feb 10, 2022';
@@ -34,7 +36,8 @@ export function changeVideo(name){
     }
     else if(name == 'danger'){
         console.log('change to danger');
-        videoSrc.setAttribute('src', './media/Danger.webm');
+        videoSrc[0].setAttribute('src', './media/Danger.webm');
+        videoSrc[1].setAttribute('src', './media/Danger.mov');
         video.setAttribute('poster', './images/artGallery/danger.jpeg');
         title.innerText = 'Danger';
         smallTitle.innerHTML = 'Filming · Editing · Directing · Acting <br>Feb 10, 2021';
@@ -43,7 +46,8 @@ export function changeVideo(name){
     }
     else if(name == 'family'){
         console.log('change to family');
-        videoSrc.setAttribute('src', './media/HardFamily.webm');
+        videoSrc[0].setAttribute('src', './media/HarderFamily.webm');
+        videoSrc[1].setAttribute('src', './media/HarderFamily.mp4');
         video.setAttribute('poster', './images/artGallery/family.jpg');
         title.innerText = 'Harder Family';
         smallTitle.innerHTML = 'Filming · Editing · Directing <br>Feb 10, 2020';
